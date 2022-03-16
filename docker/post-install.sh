46eafc87dcb1713affc8f61ee323cb83e11da978
dzoladz <derek@derekzoladz.com>
2022-03-15 21:06:38 -0400

    initial docker

---
+++ docker/post-install.sh
@@ -0,0 +1,1 @@
+docker exec -it -w /var/www/html/coral coral-app /bin/bash -c "chmod 755 common auth/admin licensing/admin management/admin organizations/admin reports/admin resources/admin usage/admin"
